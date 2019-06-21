4e4bc6e031cc0cce047e52a94f1451419059f06d
Mateusz Kwieciński <mateusz.kwiecinski@tooploox.com>
2019-06-21 13:50:40 +0200

    Simplify

--- buildSrc/src/main/kotlin/com/help/plugins/AndroidLibraryPlugin.kt
+++ buildSrc/src/main/kotlin/com/help/plugins/AndroidLibraryPlugin.kt
@@ -1,17 +1,12 @@
 package com.help.plugins
 
-import com.android.build.gradle.LibraryExtension
 import com.help.plugins.internal.BaseAndroidPlugin
 import org.gradle.api.Project
 
 internal class AndroidLibraryPlugin : BaseAndroidPlugin() {
 
-    override fun apply(project: Project) = with(project) {
-        pluginManager.apply("com.android.library")
+    override fun apply(project: Project) {
+        project.pluginManager.apply("com.android.library")
         super.apply(project)
-
-        extensions.getByType(LibraryExtension::class.java).libraryVariants.all { variant ->
-            variant.generateBuildConfigProvider.configure { it.enabled = false }
-        }
     }
 }
--- buildSrc/src/main/kotlin/com/help/plugins/internal/BaseAndroidPlugin.kt
+++ buildSrc/src/main/kotlin/com/help/plugins/internal/BaseAndroidPlugin.kt
@@ -4,43 +4,33 @@ import com.android.build.gradle.TestedExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 internal abstract class BaseAndroidPlugin : Plugin<Project> {
 
     override fun apply(project: Project) = with(project) {
         pluginManager.apply("kotlin-android")
-        pluginManager.apply(ConfigurationPlugin::class.java)
+        with(repositories){
+            google()
+            jcenter()
+        }
 
         extensions.getByType(TestedExtension::class.java).apply {
-            compileSdkVersion(COMPILE_SDK_VERSION)
+            compileSdkVersion(28)
             defaultConfig.apply {
-                minSdkVersion(MIN_SDK_VERSION)
-                targetSdkVersion(COMPILE_SDK_VERSION)
-                setTestInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+                minSdkVersion(26)
+                targetSdkVersion(28)
             }
-            dexOptions {
-                it.setPreDexLibraries(true)
-            }
-
-            configureSourceSets()
 
             compileOptions.apply {
                 sourceCompatibility = JavaVersion.VERSION_1_8
                 targetCompatibility = JavaVersion.VERSION_1_8
             }
         }
-        Unit
-    }
 
-    private fun TestedExtension.configureSourceSets() {
-        sourceSets.all { set ->
-            val withKotlin = set.java.srcDirs.map { it.path.replace("java", "kotlin") }
-            set.java.setSrcDirs(set.java.srcDirs + withKotlin)
+        tasks.withType(KotlinCompile::class.java).all {
+            it.kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
         }
     }
 
-    companion object {
-        private const val COMPILE_SDK_VERSION = 28
-        private const val MIN_SDK_VERSION = 26
-    }
 }
